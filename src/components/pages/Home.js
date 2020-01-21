267e88ad667a765c6237598d0219aeceb00e0a07
mr-jameson <elliot.jameson4@gmail.com>
2020-01-21 16:24:16 +1100

    added content to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.section`
+    text-align: center;
+`;
+
+const subHeading = styled.section`
+    text-align: center;
+`;
 
 
 class Home extends React.Component {
 
 
     render () {
-        return <h1>Oakbrook Yass Valley</h1>
+        return (
+            <>
+            <Heading>
+                <h1>Oakbrook</h1>
+            </Heading>
+            <subHeading>
+                <h2>All Australian, organically managed olive grove and trufferie - Yass Valley, New South Wales.</h2>
+            </subHeading>
+            </>
+        );
     }
 }
 
