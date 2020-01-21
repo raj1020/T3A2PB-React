738a4963cb0d00825e05a8eaff591fd6032e184a
mr-jameson <elliot.jameson4@gmail.com>
2020-01-21 15:21:50 +1100

    setup healthBenefits page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from "react-router-dom";
+import {BrowserRouter, Route, Link, Switch} from "react-router-dom";
 import Contact from './pages/Contact';
 //import NavBar from './NavBar';
 import Home from './pages/Home';
@@ -9,34 +9,33 @@ import OurStory from './pages/OurStory';
 import Products from './pages/Products';
 import Retailers from './pages/Retailers';
 import ShowProduct from './pages/ShowProduct';
-
-
+// import Styled from 'styled-components';
 
  class App extends React.Component {
+     state = { location: "home" }
 
     render () {
         return (
             <>
             <BrowserRouter> 
-                <Link to="/">Home</Link> <span> </span> 
-                <Link to="/ourstory">Our Story</Link>  <span> </span> 
-                <Link to="/products">Products</Link>  <span> </span> 
-                <Link to="/healthbenefits">Health Benefits</Link> <span> </span> 
-                <Link to="/retailers">Retailers</Link>  <span> </span> 
-                <Link to="/contact">Contact</Link>  <span> </span> 
-                <Link to="/checkout">Check Out</Link>  
-                <Route exact path = "/" component = {Home} />
-                <Route exact path = "/contact" component = {Contact} />
-                <Route exact path = "/checkout" component = {CheckOut} />
-                <Route exact path = "/healthbenefits" component = {HealthBenefits} />
-                <Route exact path = "/ourstory" component = {OurStory} />
-                <Route exact path = "/products" component = {Products} />
-                <Route exact path = "/retailers" component = {Retailers} />
-                <Route exact path = "/showproduct" component = {ShowProduct} />
-
-
+                    <Link to="/">Home</Link> <span> </span> 
+                    <Link to="/ourstory">Our Story</Link>  <span> </span> 
+                    <Link to="/products">Products</Link>  <span> </span> 
+                    <Link to="/healthbenefits">Health Benefits</Link> <span> </span> 
+                    <Link to="/retailers">Retailers</Link>  <span> </span> 
+                    <Link to="/contact">Contact</Link>  <span> </span> 
+                    <Link to="/checkout">Check Out</Link>  
+                <Switch>
+                    <Route exact path = "/" component = {Home} />
+                    <Route exact path = "/contact" component = {Contact} />
+                    <Route exact path = "/checkout" component = {CheckOut} />
+                    <Route exact path = "/healthbenefits" component = {HealthBenefits} />
+                    <Route exact path = "/ourstory" component = {OurStory} />
+                    <Route exact path = "/products" component = {Products} />
+                    <Route exact path = "/retailers" component = {Retailers} />
+                    <Route exact path = "/showproduct" component = {ShowProduct} />
+                </Switch>
             </BrowserRouter>  
-                
             </>
         )
     }
