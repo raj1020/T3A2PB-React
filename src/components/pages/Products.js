81d6ee2219c90ff346547ec0c2052d83d2a4a6bc
thomasalfonso <thomas.alfonso.email@gmail.com>
2020-01-22 11:40:09 +1100

    added api configuration

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import api from '../../api/api'
 
 class Products extends React.Component {
 
+    componentDidMount = async ()=>{
+        const response = await api.get('/user')
+        console.log(response)
+    }
 
     render () {
+        
         return <h1>Fantastic and Fabulous Products</h1>
     }
 }
