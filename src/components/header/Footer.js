7b6de6f2610f7225a6be74b71269760b2e2cda37
mr-jameson <elliot.jameson4@gmail.com>
2020-01-24 15:37:43 +1100

    integrated colored, styled footer

--- src/components/header/Footer.js
+++ src/components/header/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import logo from '../../images/logo3.jpg'
+// import fbLogo from '../../images/fbLogo.png'
 import './../../styles/index.css';
+import Link from '@material-ui/core/Link';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 class Footer extends React.Component {
 
@@ -8,7 +11,15 @@ class Footer extends React.Component {
         return (
             <>
                 <div className="footerLayer1">
-                    
+                    <div className="SocialMedia">
+                        <Link color="inherit" href="https://www.facebook.com/Oakbrook-Yass-Valley-316970248851172/" target="_blank">
+                        <FacebookIcon fontSize="large" />
+                        </Link>{'      '}
+                        <span/>
+                        <Link color="inherit" href="https://www.instagram.com/oakbrookyassvalley/" target="_blank">
+                        <InstagramIcon fontSize="large"/>
+                        </Link>{' '}
+                    </div>
                 </div>
                 <div className="footerLayer2">
                     <div className="FooterText">Copyright Oakbrook Yass Valley 2020</div>
