061054ccff38119ce5455bd55ae43fb00920a443
mr-jameson <elliot.jameson4@gmail.com>
2020-01-28 13:00:12 +1100

    experimenting with product size/volume

--- src/components/bootstrap/BootstrapCard.js
+++ src/components/bootstrap/BootstrapCard.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroupItem';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import logoSm from '../../images/logo-sm.png'
 import './../../styles/bootstrap.css';
 
@@ -9,26 +11,26 @@ import './../../styles/bootstrap.css';
 class BootstrapCard extends React.Component {
     
     render() {
-        const { productName }=this.props;
+        const { productName, productSize }=this.props;
 
         return (
             <Card className="ProductCard">
                 <a href="/"><Card.Img variant="top" src={logoSm} /></a>
                 <Card.Body>
                     <a className="Link" href="/"><Card.Title>{productName}</Card.Title></a>
-                    <Card.Text>
-                        Olive Oil
-                    </Card.Text>
+                    <DropdownButton id="dropdown-basic-button" title={productSize}>
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </DropdownButton>
+                    <Card.Link href="#">$10.00</Card.Link>
+                    <Card.Link href="#">Buy</Card.Link>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
                     <ListGroupItem>Extra Virgin Olive Oil</ListGroupItem>
                     <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
                     <ListGroupItem>Vestibulum at eros</ListGroupItem>
                 </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">$10.00</Card.Link>
-                    <Card.Link href="#">Buy</Card.Link>
-                </Card.Body>
             </Card>
         );
     }
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -13,7 +13,7 @@ class Products extends Component {
 
     render () {
         const Products = [
-            {_id:'1', name: 'Extra Virgin Olive Oil 250ml', price: 11.00, count: 10, available: true },
+            {_id:'1', name: 'Extra Virgin Olive Oil 250ml', description:"Extra virgin olive oil", price: 11.00, size: "250mL", count: 10, available: true },
             {_id:'2', name: 'Extra Virgin Olive Oil 500ml', price: 16.00, count: 10, available: true },
             {_id:'3', name: 'Extra Virgin Olive Oil 3l', price: 65.00, count: 10, available: true },
             {_id:'4', name: 'Extra Virgin Olive Oil 20l', price: 230.00, count: 10, available: false },
@@ -25,7 +25,7 @@ class Products extends Component {
             <CardDeck className="ProductCardDeck">
                 { Products.map((product) =>  {
                     return (
-                    <BootstrapCard productName={product.name} />
+                    <BootstrapCard productName={product.name} productPrice={product.price} productSize={product.size}/>
                     );
                 })
             }
