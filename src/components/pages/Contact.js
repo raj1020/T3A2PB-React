94cb95c7710f8d375dcf6c4a377602cd4b290ec3
mr-jameson <elliot.jameson4@gmail.com>
2020-01-29 15:19:40 +1100

    setting up the visit us section of contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -13,25 +13,30 @@ class Contact extends React.Component {
 
             <div>
                 <h1 className="TextTitle">Contact Us</h1>
-                <h2 className="subTitle">Get in touch with us!</h2>
-                <div className="Container1">
-                <Card className="contactCard">
-                    <Card.Body>
-                        <Card.Title>Phone:</Card.Title>
-                        <Card.Text>
-                            <p className="contactText">Rick Martin 0417 229 810</p>
-                            <p className="contactText">Fiona Martin 0408 409 558</p>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="contactCard">
-                    <Card.Body>
-                    <Card.Title>Email:</Card.Title>
-                        <Card.Text>
-                            <p className="contactText">oakbrookyassvalley@gmail.com</p>
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
+                <div className="Container0">
+                    <h2 className="subTitle">Get in touch with us!</h2>
+                    <div className="Container1">
+                    <Card className="contactCard">
+                        <Card.Body>
+                            <Card.Title>Phone:</Card.Title>
+                            <Card.Text>
+                                <p className="contactText">Rick Martin 0417 229 810</p>
+                                <p className="contactText">Fiona Martin 0408 409 558</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="contactCard">
+                        <Card.Body>
+                        <Card.Title>Email:</Card.Title>
+                            <Card.Text>
+                                <p className="contactText">oakbrookyassvalley@gmail.com</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </div>
+                </div>
+                <h1 className="visitTitle">Visit Us</h1>
+                <div className="Container2">
                 </div>
             </div>
         
