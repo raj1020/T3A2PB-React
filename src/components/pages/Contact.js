c0be03c723d8068bd8f7b770974b01e44bebb243
mr-jameson <elliot.jameson4@gmail.com>
2020-01-29 13:28:06 +1100

    started contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import './../../styles/index.css';
 
 
 
@@ -10,12 +12,28 @@ class Contact extends React.Component {
         return (
 
             <div>
-        
-        <p>If you have any complaints, compliments, suggestions or questions, please feel free to contact us.</p>
-        <p>Tel: 0000000000</p>
-        <p>Email: oakbrook@oakbrook.com.au</p>
-
-        </div>
+                <h1 className="TextTitle">Contact Us</h1>
+                <h2 className="subTitle">Get in touch with us!</h2>
+                <div className="Container1">
+                <Card className="contactCard">
+                    <Card.Body>
+                        <Card.Title>Phone</Card.Title>
+                        <Card.Text>
+                            Rick Martin 0417 229 810<br />
+                            Fiona Martin 0408 409 558<br />
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card className="contactCard">
+                    <Card.Body>
+                    <Card.Title>Email</Card.Title>
+                        <Card.Text>
+                            oakbrookyassvalley@gmail.com
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                </div>
+            </div>
         
         )
 
