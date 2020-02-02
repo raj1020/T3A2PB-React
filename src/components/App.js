686a32d8d403e97537f259fa31da7c5d7824309a
mr-jameson <elliot.jameson4@gmail.com>
2020-02-02 19:28:32 +1100

    created confirmation page

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import Home from './pages/Home';
 import HealthBenefits from './pages/HealthBenefits';
 import OurStory from './pages/OurStory';
 import Products from './pages/Products';
-// import Retailers from './pages/Retailers';
+import Confirmation from './pages/Confirmation';
 import ShowProduct from './pages/ShowProduct';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import NavBar from './mui/NavBar';
 import Footer from './header/Footer';
 import Header from './header/Header';
-import Cart from './pages/Cart';
+// import Cart from './pages/Cart';
 import CartPage from './pages/CartPage';
 import CheckOut from './pages/CheckOut';
 // import CheckOutPage from './pages/CheckOutPage';
@@ -35,7 +35,7 @@ import CheckOut from './pages/CheckOut';
                         <Route exact path = "/healthbenefits" component = {HealthBenefits} />
                         <Route exact path = "/ourstory" component = {OurStory} />
                         <Route exact path = "/products" component = {Products} />
-                        {/* <Route exact path = "/retailers" component = {Retailers} /> */}
+                        <Route exact path = "/confirmation" component = {Confirmation} />
                         <Route exact path = "/products/:id" component = {ShowProduct} />
                         <Route exact path = "/checkout" component = {CheckOut} />
                         <Route component={Home} />
---
+++ src/components/pages/Confirmation.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import Form from 'react-bootstrap/Form';
+// import Cart from './Cart';
+import './../../styles/index.css';
+
+
+class Confirmation extends Component {
+  
+      render() {
+        return (
+        <div>
+        <h1 className="TextTitle">Order Confirmation</h1>
+            <div className="orderContainer">
+                <div className="textContainer"x>
+                    <p className="confirmationText">Thank you for your purchase.</p>
+                    <p className="confirmationText">An invoice has been sent to your email.</p>
+                    <p className="confirmationText">We will send you an email when your order has been shipped.</p>
+                </div>
+                {/* <Cart /> */}
+            </div>
+        </div>
+        );
+      }
+  }
+  
+const mapStateToProps = (state)=>{
+    return{
+        items: state.addedItems
+        //addedItems: state.addedItems
+    }
+}
+
+  export default connect(mapStateToProps)(Confirmation);
