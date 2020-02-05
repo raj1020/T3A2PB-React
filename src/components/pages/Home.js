3a91c935f90153c19d2e35c187d1d8e98d08a4f5
mr-jameson <elliot.jameson4@gmail.com>
2020-02-05 11:35:25 +1100

    added more background images

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-import image from './../../images/background1.jpg';
+import image1 from './../../images/background1.jpg';
+import image2 from './../../images/background2.jpg';
+import image3 from './../../images/background3.jpg';
+import image4 from './../../images/background4.JPG';
+// import oliveoilunsplash from './../../images/oliveoilunsplash.jpg';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
+// import HomeCarousel from './../mui/Carousel'
 // import Header from './../header/Header';
 
-const Container = styled.section`
+const Container1 = styled.section`
     text-align: center;
     padding: 10vh;
-    background-image: url(${image});
+    background-image: url(${image1});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    min-height: 100vh;
+    position: relative;
+    padding-bottom: 72px;
+    font-family: 'Geometria-Medium', sans-serif;
+`;
+
+const Container2 = styled.section`
+    text-align: center;
+    // padding: 10vh;
+    background-image: url(${image2});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    min-height: 100vh;
+    position: relative;
+    padding-bottom: 72px;
+    font-family: 'Geometria-Medium', sans-serif;
+`;
+
+const Container3 = styled.section`
+    text-align: center;
+    // padding: 10vh;
+    background-image: url(${image3});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    min-height: 100vh;
+    position: relative;
+    padding-bottom: 72px;
+    font-family: 'Geometria-Medium', sans-serif;
+`;
+
+const Container4 = styled.section`
+    text-align: center;
+    // padding: 10vh;
+    background-image: url(${image4});
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
@@ -40,15 +84,24 @@ class Home extends React.Component {
     render () {
         return (
             <>
-            {/* <Header /> */}
-            <Container>
+            <Container1>
                 <div>
                     <HomeTitle>OAKBROOK</HomeTitle>
                     <SubTitle>Yass Valley</SubTitle>
-                    <HomeText>All Australian, organically managed olive grove and trufferie.</HomeText>
+                    <HomeText>Rich & Robust, Cold Pressed Extra Virgin Olive Oil</HomeText>
                     <Link to="/products"><Button variant="primary">SHOP</Button></Link>
                 </div>
-            </Container>
+            </Container1>
+            <Container3>
+                <div className='subTitle3'>All Australian owned and produced, organically managed olive grove and trufferie.</div>
+            </Container3>
+            <Container4>
+                <div className='subTitle3'>In the cool climate of Vass Valley, southern New South Wales, we produce our boutique robust olive oil.</div>
+            </Container4>
+            <Container2>
+                <div className='subTitle2'>Our extra virgin olive oil is unfiltered with no additives. <br /><br />We endeavour to be environmentally sustainable using biological pest and weed control. </div>
+                <Link to="/products"><Button variant="primary">SHOP</Button></Link>
+            </Container2>
             </>
         );
     }
